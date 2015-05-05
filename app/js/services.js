6eb1180146f9de3e40c7c8399ad73b11a208a4bf
christian-kocke <christian.kocke@gmail.com>
2015-05-05 15:11:51 +0700

    sockets

--- app/js/services.js
+++ app/js/services.js
@@ -5,6 +5,31 @@
 var datingService = angular.module('datingServices', ['ngResource']);
 
 
+datingService.factory('socket', function ($rootScope) {
+    var socket = io.connect('http://127.0.0.1:3000/');
+    return {
+        on: function (eventName, callback) {
+            socket.on(eventName, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    callback.apply(socket, args);
+                });
+            });
+        },
+        emit: function (eventName, data, callback) {
+            socket.emit(eventName, data, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    if (callback) {
+                        callback.apply(socket, args);
+                    }
+                });
+            })
+        }
+    };
+});
+
+
 datingService.factory('ResetService', function ($http, RESOURCE) {
 	var resetService = {};
 
